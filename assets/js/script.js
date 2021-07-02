0e7fe0b97f2522300ec0405d0c402b1be60bf0db
Mel Jack <melissa_jack@hotmail.co.uk>
2021-07-02 16:23:26 +0100

    Add player objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,25 @@ const gameBoard = (() => {
     const boardArray = ["", "", "", 
                         "", "", "",
                         "", "", "",];
-    return {boardArray};
+
+    // Update board with correct symbol
+    const addToBoard = (field) => {
+        const i = field.id;
+        field.textContent = boardArray[i];
+    }
+
+    return {boardArray, addToBoard};
 })();
+
+const Player = (s) => {
+    const sign = s;
+    const makeMove = (index) => {
+        const field = document.getElementById(index);
+        gameBoard.boardArray[index] = sign;
+        gameBoard.addToBoard(field);
+    };
+    return {makeMove};
+};
+
+const playerX = Player("X");
+const playerO = Player("O");
